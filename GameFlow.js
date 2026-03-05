5f3aa0386c632212d0e6eccc2eddbfe030677ce6
Vagner <05456586088@senacrs.edu.br>
2026-03-05 14:27:24 -0300

    feat: adiciona sistema de perder vida, letras usadas e verificação de letra

--- GameFlow.js
+++ GameFlow.js
@@ -1,16 +1,69 @@
-// Vamos fazer um jogo da forca pelo terminal, ok?
-
+// vamos fazer um jogo da forca pelo terminal, ok?
 
 const readline = require('readline-sync');
 
-// Palavras que vamos usar no jogo da forca, podem musdar se quiser
+// palavras que vamos usar no jogo da forca, podem musdar se quiser
 const palavras = ["javascript", "terminal", "computador", "programacao", "github"];
 
+let LetrasDescobertas = [];
 let tentativas = 6
-let LetrasPodeUsar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "t"];
+let LetrasUsada = [];
+let LetrasPodeUsar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Aqui vai escolher uma das palavras aleatoriamente
+//aqui vai escolher uma das palavras aleatoriamente
 const palavraAleatoria = palavras[Math.floor(Math.random() * palavras.length)];
+
+// para guardar o tamanho da palavra
+let tamanhoPalavra = [];
+
+// função para mostrar a palavra escolhida ocultada
+function mostrarPalavraOculta() {
+for (let i = 0; i < palavraAleatoria.length; i++) {
+    tamanhoPalavra.push('_');
+    }
+    // o join serve para mudar o separador entre os elementos, nesse caso ele é um espaço vazio
+    let palavraOculta = tamanhoPalavra.join(" ");
+    console.log(palavraOculta)
+}
+
+//função de perder vida
+function PerderVida(){
+
+tentativas--;
+
+    if (tentativas > 0) {
+        console.log("Você errou! Restam " + tentativas + " tentativas.");
+    } else {
+        console.log("Você perdeu! Fim de jogo.");
+    }
+}
+
+
+// Texto inicial do jogo
+console.log("Olá, você está em um Jogo da Forca, digita a palavra certa ou morra! 🪢")
+console.log(" ")
+console.log("❌ Você tem 6, a cada de letra errada você perde uma vida! ☠️")
+console.log("____________________________ / / ______________________________________")
+console.log(" ")
+
+// iniciando jogo
+console.log("Jogo iniciado, descubra a palavra ou morra!!! ☠️")
+console.log(mostrarPalavraOculta())
+
+console.log("======================================================")
+
+let letra = readline.question("Digite uma letra: ");
+
+// verificar se a letra existe na palavra
+if (!palavraAleatoria.includes(letra)) {
+
+    PerderVida();
+
+} else {
+
+    console.log("Boa! A letra existe na palavra.");
+
+}
 
 
 
